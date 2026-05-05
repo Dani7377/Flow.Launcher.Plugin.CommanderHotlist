6bec8f4684b8c235ab04a6a0b58815980766baf6
Dani7377 <daniel.mariuta@yahoo.com>
2026-05-05 23:07:35 +0300

    Prefix the result title with the source tag instead of displaying it in the subtitle

--- Flow.Launcher.Plugin.CommanderHotlist/HotlistResultBuilder.cs
+++ Flow.Launcher.Plugin.CommanderHotlist/HotlistResultBuilder.cs
@@ -12,16 +12,14 @@ namespace Flow.Launcher.Plugin.CommanderHotlist
             IReadOnlyDictionary<ToolType, string> subtitleTagLookup, 
             bool showSourceTag, IReadOnlyDictionary<ToolType, ToolConfig> toolLookup)
         {
-            string subtitle = showSourceTag
-                ? entry.Path + "  " + subtitleTagLookup[entry.ToolType]
-                : entry.Path;
+            string title = showSourceTag ? subtitleTagLookup[entry.ToolType] + " " + entry.Name : entry.Name;
 
             var tool = toolLookup[entry.ToolType];
 
             Result result = new Result
             {
-                Title = entry.Name,
-                SubTitle = subtitle,
+                Title = title,
+                SubTitle = entry.Path,
                 IcoPath = "Images\\app.png",
                 Action = _ => CommanderLauncher.Launch(entry.Path, tool, context)
             };
